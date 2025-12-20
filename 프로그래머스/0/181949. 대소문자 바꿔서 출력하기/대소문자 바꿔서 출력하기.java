fa1963fe0c9d27d3eeff975064b054e97cc7e384
Choi Sujin <43837946+cupucharm@users.noreply.github.com>
2025-12-20 17:34:44 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 171.59 ms, Memory: 68.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        char[] ch = new char[a.length()];
+        
+        for (int i = 0; i < a.length(); i++) {
+            ch[i] = a.charAt(i);
+        }
+        
+        int n = 'a' - 'A';
+        
+        for(int i = 0; i < a.length(); i++) {
+            if (ch[i] < 'a') {
+                ch[i] += n;
+            } else {
+                ch[i] -= n;
+            }
+            System.out.print(ch[i]);
+        }
+    }
+}
